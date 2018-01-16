15e0a822204b4587c353fab4e33970e0f371ab32
aleber <aleber@a1413>
2018-01-16 16:49:41 +0100

    hola

--- AceptaElReto/src/aceptaelreto/AceptaElReto.java
+++ AceptaElReto/src/aceptaelreto/AceptaElReto.java
@@ -16,6 +16,9 @@ public class AceptaElReto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hola");
+        
     }
     
 }
